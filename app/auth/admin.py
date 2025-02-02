431b3ac5bc55852ec7751c2c627e4f87ea1210f9
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-02 23:44:41 +0300

    add auth routes for admin

---
+++ app/auth/admin.py
@@ -0,0 +1,285 @@
+import uuid
+from jwt import InvalidTokenError
+import pymysql
+from sqlmodel import SQLModel, Session
+from fastapi import APIRouter, Depends, HTTPException, status, Form, BackgroundTasks
+from pathlib import Path
+from dotenv import load_dotenv
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import timedelta, datetime, timezone
+from .model_schema import AdminCreate, AdminPublic, Admin
+import jwt
+from secrets import token_urlsafe
+import os
+from ..dependencies import SessionDependency
+
+router = APIRouter(
+    prefix='/admin',
+    tags=['admin']
+)
+
+current_dir = Path(__file__).resolve().parent if __file__ in locals() else Path.cwd()
+env_directory = current_dir / '.env'
+load_dotenv(env_directory)
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='admin/token')
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITH= os.getenv('ALGORITHM')
+sender_email = os.getenv('SENDER_EMAIL')
+email_password = os.getenv('EMAIL_PASSWORD')
+ACCESS_TOKEN_EXPIRE = 7
+
+
+
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+class ForgottenPassword(SQLModel):
+    email: str
+
+
+def verify_password(plain_password: str, hash_password: str):
+    return bcrypt_context.verify(plain_password, hash_password)
+
+
+
+def get_hashed_password(password: str):
+    return bcrypt_context.hash(password)
+
+
+def validate_reset_password(token: str, db: pymysql.connections.Connection, now:datetime):
+    with db.cursor() as cursor:
+        query = """
+        SELECT * FROM e_waste.password_reset WHERE reset_token = %s
+        AND used_reset_token = %s
+        AND reset_token_expiry > %s
+        """
+        cursor.execute(query, (token, 0, now))
+        valid_token = cursor.fetchone()
+        if valid_token:
+            return valid_token
+        return False
+
+
+def get_user_by_email(db: pymysql.connections.Connection, email: str):
+    with db.cursor() as cursor:
+        query = """
+        SELECT * FROM e_waste.admin WHERE email = %s
+        """
+        cursor.execute(query, email)
+        admin = cursor.fetchone()
+        if admin:
+            return admin
+        return None
+
+def get_user(phone_number: str, email: str, db: pymysql.connections.Connection):
+    with db.cursor() as cursor:
+        query = "SELECT * FROM e_waste.admin WHERE phone_number = %s OR  email = %s"
+        cursor.execute(query, (phone_number, email))
+        user = cursor.fetchone()
+        if user:
+            return user
+        return None
+
+def authenticate_user(phone_number: str, password: str, db: pymysql.connections.Connection):
+    # user = get_user(phone_number, email, db)
+    with db.cursor() as cursor:
+        query = """
+        SELECT * FROM e_waste.admin WHERE phone_number = %s
+        """
+        cursor.execute(query, phone_number)
+        admin = cursor.fetchone()
+        if not admin:
+            return  False
+        if not verify_password(password, admin['hash_password']):
+            return  False
+        return admin
+
+def create_reset_token(admin_id: str , db: pymysql.connections.Connection):
+    EXPIRY_DURATION = 24
+    reset_token = token_urlsafe(32)
+    reset_token_expiry = datetime.now() + timedelta(hours=EXPIRY_DURATION)
+    reset_password_id = uuid.uuid4()
+    with db.cursor() as cursor:
+        try:
+            query = """
+            INSERT INTO e_waste.password_reset(id, user_id, reset_token, reset_token_expiry, user_type)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+            cursor.execute(query, (reset_password_id, admin_id, reset_token, reset_token_expiry, 'admin'))
+            db.commit()
+            select_query = """
+            SELECT * FROM e_waste.password_reset WHERE id = LAST_INSERT_ID()
+            """
+            cursor.execute(select_query)
+            return cursor.fetchone()
+        except Exception as e:
+            print(e)
+            return None
+
+@router.post('/forgot-password')
+def forgot_password(forgot_password_request: ForgottenPassword, background_tasks: BackgroundTasks, db: pymysql.connections.Connection = SessionDependency):
+    user = get_user_by_email(db, forgot_password_request.email)
+    if not user:
+        return {
+            'message': 'User not found',
+            'result': 'fail'
+        }
+    password_reset = create_reset_token(user['id'], db)
+    print(f'Password reset {password_reset}')
+    print(f"User!!! {user}")
+    if password_reset:
+        # send_reset_password_mail(background_tasks, user['email'], user['full_name'], password_reset['reset_token'], email_password, sender_email)
+        return  {
+            'message': 'Reset token sent to email, kindly reset password',
+            'result': 'success',
+
+        }
+    return {
+        'message': 'Something went wrong, please try again',
+        'result': 'fail',
+    }
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expires_in = datetime.now(timezone.utc) + expires_delta
+    else:
+        expires_in = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expires_in})
+    jwt_encoded = jwt.encode(to_encode, SECRET_KEY, ALGORITH)
+    return jwt_encoded
+
+class OAuth2PasswordRequestFormCustom(OAuth2PasswordRequestForm):
+    def __init__(self,
+                phone_number: str = Form(...),
+                 password: str = Form(...),
+                ):
+            super().__init__(username=phone_number, password=password)
+
+    def get_current_active_user(self: str = Depends(oauth2_scheme)):
+        credential_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate credentials',
+
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+        try:
+            payload = jwt.decode(self, SECRET_KEY, ALGORITH)
+            full_name: str = payload.get('sub')
+            user_id: int = payload.get('user_id')
+            exp: str = payload.get('exp')
+            email: str = payload.get('email')
+            if full_name is None or user_id is None:
+                raise credential_exception
+            return {'full_name': full_name, 'user_id': user_id, 'exp': exp, 'email': email}
+        except InvalidTokenError:
+            raise credential_exception
+
+
+    # create admin
+@router.post("/token", response_model=Token)
+def login_for_access_token(form_data: OAuth2PasswordRequestFormCustom = Depends(), db: pymysql.connections.Connection = SessionDependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise  HTTPException (
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail='Wrong credentials',
+            headers={"WWW-authorization": 'Bearer'}
+        )
+
+    access_token_expires = timedelta(days = ACCESS_TOKEN_EXPIRE)
+    access_token = create_access_token(data={"sub": user['full_name'], "user_id": user['id'], "email": user['email']}, expires_delta=access_token_expires)
+    return Token(access_token=access_token, token_type='bearer')
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user( create_user_request: AdminCreate , db: pymysql.connections.Connection = SessionDependency):
+    user_in_db = get_user(create_user_request.phone_number, create_user_request.email, db)
+    if user_in_db:
+        return {
+            'message': 'Phone number or email already taken',
+            'result': 'fail'
+        }
+    else:
+        if create_user_request.password != create_user_request.confirm_password:
+            return {
+                'message': 'Passwords do not match',
+                'result': 'fail'
+            }
+        admin_id = str(uuid.uuid4())
+        hash_password = get_hashed_password(create_user_request.password)
+        with db.cursor() as cursor:
+            new_user = """
+            INSERT INTO e_waste.admin (id, full_name, email, phone_number, hash_password, created_at)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(new_user, (
+                admin_id,
+                create_user_request.full_name,
+                create_user_request.email,
+                create_user_request.phone_number,
+                hash_password,
+                datetime.now())
+            )
+            db.commit()
+            return {
+                "message": "Admin created successfully",
+                "result": "success",
+            }
+
+
+#
+@router.put('/reset-password')
+def reset_password(
+        password: str = Form(...),
+        confirm_password: str = Form(...),
+        token: str = Form(...),
+        db:pymysql.connections.Connection = SessionDependency):
+
+    now = datetime.now()
+    everything_valid = validate_reset_password(token, db, now)
+    print(f"Everything valid {everything_valid}")
+    if everything_valid:
+        match_password = confirm_password == password
+        if not match_password:
+            return {
+                'message': 'Passwords do not match',
+                'result': 'fail'
+            }
+        hashed_new_password = get_hashed_password(password)
+        with db.cursor() as cursor:
+            try:
+                query_admin ="""
+                UPDATE e_waste.admin SET
+                hash_password = %s
+                WHERE id = %s
+                """
+                cursor.execute(query_admin, (hashed_new_password, everything_valid['user_id']))
+
+                query_password_reset = """
+                UPDATE e_waste.password_reset SET 
+                used_reset_token = %s
+                WHERE reset_token = %s
+                """
+                cursor.execute(query_password_reset, (1, token))
+                db.commit()
+                return {
+                    'message': 'Password reset successfully',
+                    'result': 'success',
+                }
+            except Exception as e:
+                print(e)
+                return {
+                    'message': 'Something went wrong, please try again',
+                    'result': 'fail'
+                }
+    return{
+        'message': 'Invalid or expired token',
+        'result': 'fail'
+    }
