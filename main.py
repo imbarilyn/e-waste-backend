efda2a3f150a7f751da12b3604234b5cd8ad6a73
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-02 23:48:58 +0300

    add path operations to main.py

--- main.py
+++ main.py
@@ -4,14 +4,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app.include_router(admin.router)
+app.include_router(aggregator.router)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 @app.on_event('startup')
 async def startup():
