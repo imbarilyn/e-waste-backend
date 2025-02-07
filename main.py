e5a03cfb2c6c1706411961e346becc35729d0094
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-07 20:33:14 +0300

    add middleware for cors

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import  create_tables
 from app.auth import admin, aggregator
 
 app = FastAPI()
+
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['*'],
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
 
 
 app.include_router(admin.router)
@@ -13,4 +23,8 @@ app.include_router(aggregator.router)
 @app.on_event('startup')
 async def startup():
   await create_tables()
+
+
+
+
 
