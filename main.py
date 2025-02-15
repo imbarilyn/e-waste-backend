8bcb1c2667a70c9843c81c9ebc49874bece5300b
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-02-15 11:11:31 +0300

    mount  staticfiles for images

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import  create_tables
-from app.auth import admin, aggregator
+from app.auth import auth_admin, auth_aggregator
 
 app = FastAPI()
+
+app.mount('/products_images/uploads', StaticFiles(directory='products_images/uploads'), name='uploads')
 
 
 app.add_middleware(
@@ -22,15 +25,11 @@ from app.routers import admin, aggregator
 
 app.include_router(aggregator.router)
 app.include_router(admin.router)
-app.include_router(aggregator.router)
 
 
 
 @app.on_event('startup')
 async def startup():
   await create_tables()
-
-
-
 
 
